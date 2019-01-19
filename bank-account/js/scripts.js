99412a8338a758fcf1b389e5fa97f97d8d2b3d6f
Angela Uwase <angelakarenzi5@gmail.com>
2019-01-19 13:28:27 +0200

    added styles

---
+++ bank-account/js/scripts.js
@@ -0,0 +1,46 @@
+var info, balance;
+
+function account(names, initial) {
+  this.Names = names;
+  this.Inital = initial;
+}
+account.prototype.bank = function(depoo) {
+  console.log(this.Initial);
+  balance = this.Inital + depoo;
+  return balance;
+};
+
+account.prototype.neb = function(client) {
+  if (this.Initial < client) {
+    alert("insufficient amount");
+  } else {
+    balance = this.Inital - client;
+
+    return balance;
+  }
+};
+
+$(document).ready(function() {
+  $("#submit1").click(function() {
+    // alert("litty")
+    var name2 = $("input#name").val();
+    var initial2 = parseInt($("input#initial").val());
+    info = new account(name2, initial2);
+    console.log(info);
+    $("#first").hide();
+    $("#second").show();
+    $("#third").show();
+  });
+  $("#submit2").click(function() {
+    var drop = parseInt($("input#money").val());
+    info.bank(drop);
+
+    alert("you deposit cash" + balance);
+  });
+  $("#submit3").click(function() {
+    var purchase = parseInt($("input#coins").val());
+    info.neb(purchase);
+    alert(" There is cash" + balance);
+  });
+});
+
